b8e5eabd913b5f1345495571a0bcce4a3bb5519d
Carol <castrocaroline11@gmail.com>
2021-01-29 21:32:47 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarsPageComponent } from './pages/account/cars-page/cars-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: CarsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarsPageComponent } from './pages/account/cars-page/cars-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    CartPageComponent,
+    CarsPageComponent,
+    ProductsPageComponent
   ],
   imports: [
     BrowserModule,
