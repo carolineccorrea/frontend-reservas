ede4ebce07659a2756fcd6c67eb6ad5d16dcd321
Carol <castrocaroline11@gmail.com>
2021-01-31 08:37:41 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,29 @@ import { CarsPageComponent } from './pages/account/cars-page/cars-page.component
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component: CarsPageComponent
-  }
+    path: '',
+    component: FramePageComponent,
+    children: [
+      { path: '', component: ProductsPageComponent },
+      { path: 'cart', component: CartPageComponent },
+    ]
+  },
+  {
+    path: 'account',
+    component: FramePageComponent,
+    children: [
+      { path: 'cars', component: CarsPageComponent },
+    ]
+  },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'reset-password', component: ResetPasswordPageComponent },
 ];
 
 @NgModule({
--- src/app/pages/account/cars-page/cars-page.component.ts
+++ src/app/pages/account/cars-page/cars-page.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cars-page',
-  templateUrl: './cars-page.component.html',
-  styleUrls: ['./cars-page.component.css']
+  templateUrl: './cars-page.component.html'
 })
 export class CarsPageComponent implements OnInit {
 
